f2debea2154663cf622b4f31ee6adbad6324e649
ssher1001 <shersumit1001@gmail.com>
2024-10-28 11:21:40 +0530

    Initial commit

---
+++ src/main/java/com/edureka/demo/edureka_demo_spring/EdurekaDemoSpringProjectApplication.java
@@ -0,0 +1,25 @@
+package com.edureka.demo.edureka_demo_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class EdurekaDemoSpringProjectApplication {
+
+	
+	public static void main(String[] args) {
+		SpringApplication.run(EdurekaDemoSpringProjectApplication.class, args);
+	}
+	
+	@GetMapping("/")
+	private String hello() 
+	{
+		return String.format("<h1>this i s the welcome page</h1>");
+
+	}
+	
+
+}
---
+++ src/test/java/com/edureka/demo/edureka_demo_spring/EdurekaDemoSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edureka.demo.edureka_demo_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EdurekaDemoSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
